10bc47066434226ed279e8963510fa29414f511f
david@DAVID-PC <david@davidjenni.com>
2018-06-16 18:09:17 -0700

    npm_cmd is already escaped

--- deploy.cmd
+++ deploy.cmd
@@ -101,9 +101,8 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd "!NPM_CMD!" install --production
+  call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
-  echo Finished npm install.
   popd
 )
 
